ff5727ec2db6a7df5c1086feee31384028aa131d
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-08-05 21:05:48 +0200

    feat: insert data of coffee shops

--- next.config.js
+++ next.config.js
@@ -5,7 +5,26 @@ const nextConfig = {
         styledComponents: true,
     },
     images: {
-        domains: ["www.ignant.com"],
+        domains: [
+            "www.ignant.com",
+            "trendland.com",
+            "cdn.th3rdwave.coffee",
+            "lh5.googleusercontent.com",
+            "images.happycow.net",
+            "www.we-heart.com",
+            "images.squarespace-cdn.com",
+            "freight.cargo.site",
+            "benrahim.de",
+            "www.thecoolguide.net",
+            "live.staticflickr.com",
+            "www.thewaytocoffee.com",
+            "sattundfroh.de",
+            "www.petitepassport.com",
+            "wheretodrink.coffee",
+            "resizer.otstatic.com",
+            "www.petitepassport.com",
+            "wheretodrink.coffee",
+        ],
     },
 };
 
