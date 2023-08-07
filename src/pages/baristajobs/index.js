14a002ee43f3af7b7870537c093773334c38f60e
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-08-07 22:10:14 +0200

    feat:adding data to job board

--- src/pages/baristajobs/index.js
+++ src/pages/baristajobs/index.js
@@ -9,7 +9,7 @@ const PageContainer = styled.div`
 `;
 
 const CoffeeShopAdsContainer = styled.div`
-    margin-top: 2rem;
+    margin-top: 0rem;
     background-image: url("https://lh3.googleusercontent.com/pw/AIL4fc_JIu-X1CQ5IeMtCEnbjVRgwrGhZoii-iC0Ya7qKyuUFy5GlavWwWSn4ZnaHqzw2jtLcD1Mh3_BG5Qqa8gefG9rTM64bw9a7YqJDV9iRpLQx7mr4tV8izSlv1BMpOSp_18ZsfrSl9_fI2MPNmlueXWnL973jAXSHXZTX2RvroVEAv9hN-0tbtbH5MEy--KGHItEVvKkbw_n5od9xELiOOng4osMkVwsH8eSyMXkUgy4cKauivNDmQ_QxtNPALHm_UgsOMVAI-bb2bkFStFAGKd8fSiFW8ZFV51R5APOw1bWMw40qzSKfQCMl0lLGw8cn_M6SoXIlyZbt3dDja48IlTgN8pl0ZwqyaK_iGd94X8nvkYR9s-ah2MUU5QP-ZDRqgfnvvbpq1_QtkWq_KNHXBhgA341TgQpG7TStyGYPdjRct-KhexpU3K4a9Y-Lp6HVK_7xr2U-zuREdmH4C6-7hmyN3pPdPIQii8JeRTJn2LVaWM3OMU8nIxGjsmxjxkqv2XqkNPG8tlgMbx8nasIpZ71111LRXrz1PRj8BBAInhtXd4S93NxnDrfLqVPiBcRVKoQzuu9RHVKRBtFNgJLSCZBdG6pMEsbxoH9sJ_7tMckoGdmaGkdrkrnDucIO62FPVeQ79BrjMWmtiVZflLFmq7LnOqRwUPEBuoB2Ve6lgFTM0HxUtRzMdL6UdaZk2h14g9y4hVEI6ovZHVBo96Vp4Gw4S3n1wJ3f9wfQMhMoJtKHT0mJWIXIWvT1ixiCQ2-B5clJznyVtRxP6ZLO4ariI28DYGeNvFRykML03yMSm-7HqLnI4J1k93unqAJzXQUkBEWPaqvA5VUuyR7gCNlbTSZgiS-HbdvCfmjCPIpnUhzWO8qBXJ5cC4ODeJ9thAscpQYx2ftZ5rUfkihLO227askhA=w1326-h884-s-no?authuser=0");
     background-size: 100%;
     min-height: 100vh;
@@ -83,21 +83,130 @@ export default function CoffeeShopAds() {
     const jobAds = [
         {
             id: 1,
-            title: "Barista at Coffee Haven",
+            title: "Barista at Cozy Cafe",
             description:
                 "We are looking for an experienced barista to join our team.",
-            location: "123 Main Street, City",
-            salary: "$12 per hour",
+            location: "Friedrichshain",
+            salary: "€12 per hour",
             qualifications: "Previous barista experience required.",
         },
         {
             id: 2,
-            title: "Barista at Espresso Delight",
+            title: "Barista at Neumman's Cafe",
             description:
-                "Join our coffee-loving team and showcase your barista skills.",
-            location: "456 Oak Avenue, Town",
-            salary: "$14 per hour",
+                "Join our coffee-loving team and showcase your barista skills. We are looking for someone who is able to work during the weekends",
+            location: "Friedrichshain",
+            salary: "€12 per hour",
             qualifications: "Barista training and latte art skills preferred.",
+        },
+        {
+            id: 3,
+            title: "Experienced Barista Needed",
+            description:
+                "Espresso Emporium is hiring an experienced barista to work in a fast-paced environment. Latte art skills and customer service experience are a plus.",
+            location: "Kreuzberg",
+            salary: "€11.50 per hour",
+            qualifications:
+                "Previous barista experience, latte art skills, customer service experience.",
+        },
+        {
+            id: 4,
+            title: "Coffee Enthusiast Wanted",
+            description:
+                "Join our team at Brewtiful Coffee House! We are looking for a coffee enthusiast who can brew the best coffee and engage with our customers.",
+            location: "Mitte",
+            salary: "€14 per hour",
+            qualifications: "Passion for coffee.",
+        },
+        {
+            id: 5,
+            title: "Barista Extraordinaire",
+            description:
+                "Do you have a love for coffee and a talent for latte art? Join our team at Caffeine Central and show off your barista skills.",
+            location: "Prenzlauer Berg",
+            salary: "€13 per hour",
+            qualifications: "Latte art skills.",
+        },
+        {
+            id: 6,
+            title: "Part-Time Barista Position",
+            description:
+                "Are you looking for a part-time job as a barista? Java Junction is hiring! Perfect for students or those looking for a flexible schedule.",
+            location: "Mitte",
+            salary: "€12 per hour",
+            qualifications: "None specified.",
+        },
+        {
+            id: 7,
+            title: "Full-Time Barista Role",
+            description:
+                "Become a full-time barista at Beans & Brews! We offer competitive pay and opportunities for career growth in the coffee industry.",
+            location: "Mitte",
+            salary: "€13 per hour",
+            qualifications: "None specified.",
+        },
+        {
+            id: 8,
+            title: "Coffee Shop Assistant",
+            description:
+                "Brewsters Cafe is hiring a coffee shop assistant to help with barista duties and provide excellent customer service.",
+            location: "Charlottenburg",
+            salary: "€12 per hour",
+            qualifications: "None specified.",
+        },
+        {
+            id: 9,
+            title: "Barista and Cashier",
+            description:
+                "Join the team at Cafe 35 as a barista and cashier. This role involves taking orders, making coffee, and handling transactions.",
+            location: "Neukölln",
+            salary: "€12 per hour",
+            qualifications: "None specified.",
+        },
+        {
+            id: 10,
+            title: "Specialty Coffee Barista",
+            description:
+                "If you have a passion for specialty coffee, come work with us at The Beanery. We serve only the finest single-origin beans.",
+            location: "Prenzlauer Berg",
+            salary: "€13 per hour",
+            qualifications: "None specified.",
+        },
+        {
+            id: 11,
+            title: "Barista Trainer Position",
+            description:
+                "Brew Masters is seeking an experienced barista to train new hires and maintain the quality of our coffee offerings.",
+            location: "Friedrichshain",
+            salary: "€12 per hour",
+            qualifications: "Previous barista experience, training experience.",
+        },
+        {
+            id: 12,
+            title: "Coffee Shop Manager",
+            description:
+                "We are hiring a coffee shop manager for Buzzworthy Cafe. This role involves overseeing baristas, inventory, and daily operations.",
+            location: "Mitte",
+            salary: "€15 per hour",
+            qualifications: "Management experience, leadership skills.",
+        },
+        {
+            id: 13,
+            title: "Artisanal Coffee Roastery Hiring Baristas",
+            description:
+                "Join our artisanal coffee roastery and share your love for coffee with our customers. Training provided for coffee enthusiasts.",
+            location: "Kreuzberg",
+            salary: "€12 per hour",
+            qualifications: "None specified.",
+        },
+        {
+            id: 14,
+            title: "Barista with Coffee Roasting Experience",
+            description:
+                "Roast & Sip is looking for a barista with experience in coffee roasting. Show off your skills from bean to cup.",
+            location: "Mitte",
+            salary: "€13 per hour",
+            qualifications: "Coffee roasting experience.",
         },
     ];
 
