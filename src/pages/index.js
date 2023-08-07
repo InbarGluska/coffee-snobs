f7156a09cf7f04a8644b4fa68e34ec87669daa3b
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-08-07 16:08:12 +0200

    feat: jobs page, home page corrections

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,38 @@ const HomeContainer = styled.div`
     height: 100vh;
     background-image: url("https://publiccoffeeroasters.com/wp-content/uploads/2021/06/PCR_Latteart_Argin_Quadratisch-1024x1024.jpg");
     background-size: cover;
+    background-position: center 20%;
+`;
+
+const Quote = styled.p`
+    font-size: 24px;
+    font-style: italic;
+    margin-top: 1rem;
+    border-left: 3px solid #a47e3b;
+    padding-left: 1rem;
+    color: #333;
+    position: relative;
+
+    &:before {
+        content: '"';
+        font-size: 40px;
+        font-family: Arial, sans-serif;
+        position: absolute;
+        top: -5px;
+        left: -15px;
+        color: #a47e3b;
+    }
 `;
 
 export default function Home() {
     return (
         <HomeContainer>
+            <Quote>
+                "Even bad coffee is better than no coffee at all." - David Lynch
+            </Quote>
             <StyledLink href="/coffeeshops">Coffee Shops</StyledLink>
+            <StyledLink href="/articles">My Favorites</StyledLink>
             <StyledLink href="/baristajobs">Barista Jobs</StyledLink>
-            <StyledLink href="/articles">Articles</StyledLink>
         </HomeContainer>
     );
 }
